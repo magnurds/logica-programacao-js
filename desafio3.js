edfbfe302fcdf167fcd2c0582e0bfdf030b5da52
Magno Ribeiro dos Santos <160602884+magnurds@users.noreply.github.com>
2024-03-19 14:28:07 +0000

    resolução dos desafios

---
+++ desafio3.js
@@ -0,0 +1,15 @@
+
+  const temperatura = parseFloat (window.prompt('Digite a temperatura'));
+  const escalaAtual = window.prompt('Digite a escala atual').toUpperCase();
+  const escalaFutura = window.prompt('Digite a escala futura')
+
+// parseFloat() - converte texto para tipo number com ponto decimal
+// parseInt() - converte textopara tiponumber inteiro
+// toUpperCase() - método que converte o texto em maiúsculas
+// toLowerCase() - método que converte o texto em minúsculas
+
+
+
+window.alert(`A temperatura convertida é ${temperatura + 273.15}`)
+   console.log(typeof(temperatura));
+
