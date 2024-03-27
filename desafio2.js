eb62221f1a6e5088e5d6855cbc8b79b881551d86
Magno Ribeiro dos Santos <160602884+magnurds@users.noreply.github.com>
2024-03-27 14:49:33 +0000

    iniciando desafio 4

--- desafio2.js
+++ desafio2.js
@@ -1,53 +1,30 @@
-// Crie um programa que calcule o Índice de Massa Corporal (IMC) de uma pessoa.
+function calculaIMC(peso, altura) {
 
-//Orientações:
+    const imc = peso / altura ** 2;
+    let mensagem;
+        
+    if (imc < 18.5) {
+        mensagem = `IMC: ${imc.toFixed(1)} --> Abaixo do peso`;
+    
+    } else if (imc < 25) {
+        mensagem = `IMC: ${imc.toFixed(1)} --> Peso normal`;
+    
+    } else if (imc < 30) {
+        mensagem = `IMC: ${imc.toFixed(1)} --> Sobrepeso`;
+    
+    } else if (imc < 35) {
+        mensagem = `IMC: ${imc.toFixed(1)} --> Obesidade grau I`;
+    
+    } else if (imc < 40) {
+        mensagem = `IMC: ${imc.toFixed(1)} --> Obesidade grau II`;
+    
+    } else {
+        mensagem = `IMC: ${imc.toFixed(1)} --> Obesidade grau III`;
+    }
 
-//Utilize o método prompt() do objeto window para realizar a leitura dos dados de entrada.
-//Utilize a fórmula IMC = peso / altura² para calcular o IMC.
-//Utilize a estrutura if-else para classificar o IMC de acordo com a tabela da OMS:
+    return mensagem;
 
-
-//IMC	Indicação
-//Abaixo de 18,5	Abaixo do peso
-//18,5 - 24,9	    Peso normal
-//25,0 - 29,9	    Sobrepeso
-//30,0 - 34,9	    Obesidade grau I
-//35,0 - 39,9	    Obesidade grau II
-//Acima de 40,0	    Obesidade grau III
-
-
-
-let altura, peso ;
-
-peso = window.prompt('Digite o seu peso');
-altura = window.prompt('Digite a sua altura');
-
-
- const imc = peso / altura ** 2;
-
-  
-if(imc < 18.5) {
-    window.alert(`IMC: ${imc.toFixed(1)}  -- Abaixo do Peso`);
-
-} else if(imc >= 18 && imc < 25) {
-    window.alert(`IMC: ${imc.toFixed(1) }  -- Peso Normal`);
-
-}  else if(imc >= 25 && imc < 30) {
-    window.alert(`IMC: ${imc.toFixed(1)}  -- Sobrepeso`);
-
-}  else if(imc >= 30 && imc < 35) {
-    window.alert(`IMC: ${imc.toFixed(1)}  -- Obesidade grau I`);
-
-} else if(imc >= 35 && imc < 40)  {
-    window.alert(`IMC: ${imc.tofixed(1)}  -- Obesidade grau II`)
-
-} else  {
-    window.alert(`IMC: ${imc.tofixed(1)}   -- Obesidade grau III`)
 }
-
-
-
-
 
 
 
--- desafio3.js
+++ desafio3.js
@@ -1,12 +1,66 @@
+function converteTemperatura() {
 
-  const
-  temperatura = parseFloat(window.prompt('Digite a temperatura')),
-  temperaturaAtual = window.prompt('Digite aescala atual: \nC,F,K').toLocaleUpperCase(),
-  escalaConversao = window.prompt('Digite a escala para conversão: \nC,F,K').toUpperCase();
+    const temperaturaAtual = parseFloat(window.prompt('Digite a temperatura'));
+    
+    if (!isNaN(temperaturaAtual)) {
+    
+        const
+            escalaAtual = window.prompt('Digite a escala atual: \nC -> Celsius, F -> Fahrenheit, K -> Kelvin').toUpperCase(),
+            
+            escalaConversao = window.prompt('Digite a escala para conversão: \nC -> Celsius, F -> Fahrenheit, K -> Kelvin').toUpperCase();
+    
+        if (escalaAtual === 'C') {
+    
+            if (escalaConversao === 'K') {
+                window.alert(`${temperaturaAtual} em ${escalaAtual} -> ${temperaturaAtual + 273.15} em ${escalaConversao}`);
+    
+            } else if (escalaConversao === 'F') {
+                window.alert(`${temperaturaAtual} em ${escalaAtual} -> ${(temperaturaAtual * 9 / 5) + 32} em ${escalaConversao}`);
+    
+            } else if (escalaConversao === 'C') {
+                window.alert(`Escala selecionada: ${escalaConversao}. Selecione uma escala de CONVERSÃO diferente da ATUAL.`);
+    
+            } else {
+                window.alert(`Escala selecionada: ${escalaConversao}. Essa escala não existe ou não está disponível neste programa.`);
+            }
+    
+        } else if (escalaAtual === 'F') {
+    
+            if (escalaConversao === 'K') {
+                window.alert(`${temperaturaAtual} em ${escalaAtual} -> ${(temperaturaAtual + 459.67) * 5 / 9} em ${escalaConversao}`);
+    
+            } else if (escalaConversao === 'C') {
+                window.alert(`${temperaturaAtual} em ${escalaAtual} -> ${(temperaturaAtual - 32) * 5 / 9} em ${escalaConversao}`);
+    
+            } else if (escalaConversao === 'F') {
+                window.alert(`Escala selecionada: ${escalaConversao}. Selecione uma escala de CONVERSÃO diferente da ATUAL.`);
+    
+            } else {
+                window.alert(`Escala selecionada: ${escalaConversao}. Essa escala não existe ou não está disponível neste programa.`);
+            }
+    
+        } else if (escalaAtual === 'K') {
+    
+            if (escalaConversao === 'C') {
+                window.alert(`${temperaturaAtual} em ${escalaAtual} -> ${temperaturaAtual - 273.15} em ${escalaConversao}`);
+    
+            } else if (escalaConversao === 'F') {
+                window.alert(`${temperaturaAtual} em ${escalaAtual} -> ${temperaturaAtual * 9 / 5 - 459.67} em ${escalaConversao}`);
+    
+            } else if (escalaConversao === 'K') {
+                window.alert(`Escala selecionada: ${escalaConversao}. Selecione uma escala de CONVERSÃO diferente da ATUAL.`);
+    
+            } else {
+                window.alert(`Escala selecionada: ${escalaConversao}. Essa escala não existe ou não está disponível neste programa.`);
+            }
+    
+        } else {
+            window.alert(`Escala selecionada: ${escalaAtual}. A escala ATUAL não existe ou não está disponível neste programa.`);
+        }
+    
+    } else {
+        window.alert('O número digitado para conversão não é um número válido.');
+    }
 
-  //processamento
-  if(!isNaN(temperaturaAtual)) {
+}
 
-    const
-    escalaAtual = window.prompt('Digite a escala atual: \nC,F,K')
-  }
